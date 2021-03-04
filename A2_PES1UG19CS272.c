3ef015833e078fe911ea1ba39416cce676f01da1
Mihir Kestur <mihirkestur@outlook.com>
2021-03-04 12:39:45 +0530

    progress 2

--- A2_PES1UG19CS272.c
+++ A2_PES1UG19CS272.c
@@ -12,11 +12,8 @@ typedef struct list_of_matches{
     struct list_of_matches *next_match_list, *previous_match_list;
     struct player_node *winner;
 }tournament;
-//used to create a node of type "player" which holds the number and the next node to a loser
+//used to create a node ("player") which holds the number and the next node to a loser
 static player *create_player(int number){
-    if(number == -999){
-        return NULL;
-    }
     player *new_player = (player *)malloc(sizeof(player));
     new_player->data = number;
     new_player->next_player = NULL;
@@ -49,7 +46,6 @@ static void display_second_largest(player *final_list){
         final_list = final_list->next_player;
     }
     printf("\n%d\n",second_largest);
-    free(final_list);
 }
 //this function prints the LHS of the match between two different lists in the desired format
 static void arrange_match(player *player_1, player *player_2){
@@ -71,8 +67,7 @@ static void arrange_match(player *player_1, player *player_2){
 static player *find_winner(player *player_1, player *player_2){
     player *winner,*loser,*loser_of_this_winner;
     if(player_1->data > player_2->data){ 
-        printf("%d ",player_1->data);
-        printf("%d ",player_2->data);
+        printf("%d %d ",player_1->data,player_2->data);
         winner = create_player(player_1->data);
         winner->next_player = create_player(player_2->data);
         loser = winner->next_player; 
@@ -85,8 +80,7 @@ static player *find_winner(player *player_1, player *player_2){
         }
     }
     else{
-        printf("%d ",player_2->data);
-        printf("%d ",player_1->data);
+        printf("%d %d ",player_2->data,player_1->data);
         winner = create_player(player_2->data);
         winner->next_player = create_player(player_1->data);
         loser = winner->next_player; 
@@ -103,73 +97,84 @@ static player *find_winner(player *player_1, player *player_2){
 }
 //this function prints LHS and RHS for every two new players in the array
 static void first_match(int first, int second, tournament *list){
-    printf("%d ", first);
-    printf("X ");
-    printf("%d ", second);
-    printf("= ");
-    printf("%d ",list->winner->data);
-    printf("%d ",list->winner->next_player->data); 
-    printf("\n");
+    printf("%d X %d = %d %d\n", first, second, list->winner->data, list->winner->next_player->data);
 }
 //main function to find the second largest element in the given array
 void find_second_greatest(int *numbers, int length){
     //finding the number matches to be played and initializing the adjacency list
-    int matches = ceil(log(length)/log(2)) + 1;
-    tournament *match_list_head = initialize_tournament(matches),*match_array = match_list_head;
-    //setting the last value to -999 so that we get to know all values in array has played match
-    numbers[length] = -999;
+    int matches = ceil(log(length)/log(2)) + 1, flag = 0;
+    tournament *match_list_head = initialize_tournament(matches),*match_list = match_list_head;
+    if(length % 2 != 0){
+        length = length - 1;
+        flag = 1;
+    }
     //accessing all elements in the array
     for(int i = 0; i < length; i = i + 2){
         
-        match_array = match_list_head;
+        match_list = match_list_head;
         //comparing the two numbers and creating a list
         if(numbers[i] > numbers[i+1]){
-            match_array->winner = create_player(numbers[i]);
-            match_array->winner->next_player = create_player(numbers[i+1]);
+            match_list->winner = create_player(numbers[i]);
+            match_list->winner->next_player = create_player(numbers[i+1]);
         }
         else{
-            match_array->winner = create_player(numbers[i+1]);
-            match_array->winner->next_player = create_player(numbers[i]);
+            match_list->winner = create_player(numbers[i+1]);
+            match_list->winner->next_player = create_player(numbers[i]);
         }
         //arrange match between two numbers if two numbers are there if only 1 don't arrange match
-        if(match_array->winner->next_player != NULL){ 
-            first_match(numbers[i],numbers[i+1],match_array);
+        if(match_list->winner->next_player != NULL){ 
+            first_match(numbers[i],numbers[i+1],match_list);
         }
         //arrange match between two different match lists if another match list exists
-        match_array = match_list_head->next_match_list;
-        while(match_array->winner != NULL){
-            arrange_match(match_array->previous_match_list->winner, match_array->winner);
-            match_array->winner = find_winner(match_array->previous_match_list->winner, match_array->winner);
-            match_array->previous_match_list->winner = NULL;
-            match_array = match_array->next_match_list;
+        match_list = match_list_head->next_match_list;
+        while(match_list->winner != NULL){
+            arrange_match(match_list->previous_match_list->winner, match_list->winner);
+            match_list->winner = find_winner(match_list->previous_match_list->winner, match_list->winner);
+            match_list->previous_match_list->winner = NULL;
+            match_list = match_list->next_match_list;
         }
-        match_array->winner = match_array->previous_match_list->winner;
-        match_array->previous_match_list->winner = NULL;
+        match_list->winner = match_list->previous_match_list->winner;
+        match_list->previous_match_list->winner = NULL;
+    }
+    if(flag){
+        match_list = match_list_head;
+        match_list->winner = create_player(numbers[length]);
+        //arrange match between two different match lists if another match list exists
+        match_list = match_list_head->next_match_list;
+        while(match_list->winner != NULL){
+            arrange_match(match_list->previous_match_list->winner, match_list->winner);
+            match_list->winner = find_winner(match_list->previous_match_list->winner, match_list->winner);
+            match_list->previous_match_list->winner = NULL;
+            match_list = match_list->next_match_list;
+        }
+        match_list->winner = match_list->previous_match_list->winner;
+        match_list->previous_match_list->winner = NULL;
     }
     //once all the elements in the array has played at least one match
-    match_array = match_list_head;
+    match_list = match_list_head;
     player *final_match_list;
-    while(match_array != NULL){
-        if(match_array->winner != NULL){
-            final_match_list = match_array->winner;
+    while(match_list != NULL){
+        if(match_list->winner != NULL){
+            final_match_list = match_list->winner;
             break;
         }
-        match_array = match_array->next_match_list;
+        match_list = match_list->next_match_list;
     }
-    match_array = match_array->next_match_list;
-    while(match_array != NULL){
-        if(match_array->winner != NULL){
-            if(match_array->winner->data > final_match_list->data){
-                arrange_match(match_array->winner,final_match_list);
-                match_array->winner = find_winner(match_array->winner,final_match_list);
-                final_match_list = match_array->winner;
+    match_list = match_list->next_match_list;
+    while(match_list != NULL){
+        if(match_list->winner != NULL){
+            if(match_list->winner->data > final_match_list->data){
+                arrange_match(match_list->winner,final_match_list);
+                match_list->winner = find_winner(match_list->winner,final_match_list);
+                final_match_list = match_list->winner;
             }
             else{
-                arrange_match(final_match_list,match_array->winner);
-                final_match_list = find_winner(final_match_list,match_array->winner);
+                arrange_match(final_match_list,match_list->winner);
+                final_match_list = find_winner(final_match_list,match_list->winner);
             }
         }
-        match_array = match_array->next_match_list;
+        match_list = match_list->next_match_list;
     }
+    //since we know first element is the greatest element, passing the next node reduces the number of comparisons
     display_second_largest(final_match_list->next_player);
 }
--- sanity_client.c
+++ sanity_client.c
@@ -4,36 +4,10 @@
 #include <stdlib.h>
 int main()
 {
-    int n;
-    int a[1000000];
+    int n = 11;
+    int a[] = {1,2,3,4,5,6,7,8,9,10,11};
 
-    //find_second_greatest(a,n);
-
-    //testing only
-    srand(time(0));
-    n = rand()%10;
-    for(int i = 0; i < n; i++){
-        a[i] = rand()%10;
-    }
     find_second_greatest(a,n);
-    int high = a[0], second_high = a[1];
-    if(a[0] > a[1]){
-        high = a[0];
-        second_high = a[1];
-    }
-    else{
-        high = a[1];
-        second_high = a[0];
-    }
-    for(int i=2;i<n;i++){
-        if(a[i]>high){
-            second_high = high;
-            high = a[i];
-        }
-        else if(a[i]>second_high){
-            second_high = a[i];
-        }
-    }
-    printf("highest is = %d\nsecond highest is = %d\n",high,second_high);
+
     return 0;
 }
